13db9aa79749eff6ef5969e42ede52554c3344ac
gitul0515 <gitul0515@gmail.com>
2022-02-10 22:37:53 +0900

    css 수정

--- 드림코딩/react-basic/youtube/src/components/video.jsx
+++ 드림코딩/react-basic/youtube/src/components/video.jsx
@@ -2,11 +2,17 @@ export default function Video(props) {
   const topVideo = props.topVideo;
   return (
     <li className="video">
-      <img className="video__img" src={topVideo.snippet.thumbnails.default.url} alt="" />
-      <div className="video__description">
-        <h4 className="video__title">{topVideo.snippet.title}</h4>
-        <span className="author">{topVideo.snippet.channelTitle}</span>
-      </div>
+      <a href="#sd">
+        <img 
+          className="video__img" 
+          src={topVideo.snippet.thumbnails.medium.url} 
+          alt="video 이미지"
+        />
+        <div className="video__description">
+          <h5 className="video__title">{topVideo.snippet.title}</h5>
+          <span className="video__author">{topVideo.snippet.channelTitle}</span>
+        </div>
+      </a>
     </li>
   )
 }
