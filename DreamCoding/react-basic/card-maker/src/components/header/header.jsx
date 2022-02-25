5e82afeb02a2825790849c1adc3a088b6dedcdc9
gitul0515 <gitul0515@gmail.com>
2022-02-25 21:28:09 +0900

    add firebase

--- DreamCoding/react-basic/card-maker/src/components/header/header.jsx
+++ DreamCoding/react-basic/card-maker/src/components/header/header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './header.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <img src="images/logo.png" alt="로고 이미지"/>
       <h1>Business Card Maker</h1>
+      <button className={styles.signOutButton}>
+        <FontAwesomeIcon icon={faRightFromBracket} />
+      </button>
     </header>
   );
 };
