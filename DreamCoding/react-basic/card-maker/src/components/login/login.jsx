7f2305095b5cfd64cef0afbc3420376dc41115e0
gitul0515 <gitul0515@gmail.com>
2022-02-25 22:36:26 +0900

    add firebase authorization ()

--- DreamCoding/react-basic/card-maker/src/components/login/login.jsx
+++ DreamCoding/react-basic/card-maker/src/components/login/login.jsx
@@ -3,14 +3,27 @@ import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
-const Login = () => {
+const Login = ({authService}) => {
+  const onLogin = (event) => {
+    authService
+    .login(event.currentTarget.textContent)
+    .then(console.log);
+
+  };
+
   return (
     <section className={styles.login}>
       <Header />
       <section className={styles.main}>
         <h1>Login</h1>
-        <button>Google</button>
-        <button>Github</button>
+        <ul>
+          <li>
+            <button onClick={onLogin}>Google</button>
+          </li>
+          <li>
+            <button onClick={onLogin}>Github</button>
+          </li>
+        </ul>
       </section>
       <Footer />
     </section>
